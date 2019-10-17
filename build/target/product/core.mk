2e1336cc495b6a79170158ebec27baf2ea58dadf
Eric <westcoastroms@gmail.com>
2019-10-16 19:25:20 -0700

    first commit

---
+++ build/target/product/core.mk
@@ -0,0 +1,88 @@
+#
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Base configuration for communication-oriented android devices
+# (phones, tablets, etc.).  If you want a change to apply to ALL
+# devices (including non-phones and non-tablets), modify
+# core_minimal.mk instead.
+
+PRODUCT_PACKAGES += \
+    BasicDreams \
+    Browser \
+    Calculator \
+    Calendar \
+    CalendarProvider \
+    CaptivePortalLogin \
+    CertInstaller \
+    Contacts \
+    DeskClock \
+    DocumentsUI \
+    DownloadProviderUi \
+    Email \
+    Exchange2 \
+    ExternalStorageProvider \
+    FusedLocation \
+    InputDevices \
+    KeyChain \
+    Keyguard \
+    LatinIME \
+    Launcher2 \
+    ManagedProvisioning \
+    PicoTts \
+    PacProcessor \
+    libpac \
+    PrintSpooler \
+    ProxyHandler \
+    QuickSearchBox \
+    Settings \
+    SharedStorageBackup \
+    Telecom \
+    TeleService \
+    VpnDialogs \
+    MmsService
+
+# strongSwan
+PRODUCT_PACKAGES += \
+    charon \
+    libcharon \
+    libhydra \
+    libstrongswan
+
+# e2fsprog
+PRODUCT_PACKAGES += \
+    e2fsck \
+    resize2fs
+
+# libexifa
+PRODUCT_PACKAGES += \
+    libexifa
+
+# libjpega
+PRODUCT_PACKAGES += \
+    libjpega
+
+# KeyUtils
+PRODUCT_PACKAGES += \
+    libkeyutils
+
+PRODUCT_PACKAGES += \
+    libdmverity_hashgen
+    
+PRODUCT_PACKAGES += \
+    brctl \
+    ebtables
+    
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
